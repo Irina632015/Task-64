0db0a219372bce97a13f730fd337adfecedb824d
Irina632015 <irina.632015@yandex.ru>
2023-02-11 23:01:25 +0400

    Семинар 9 Задача 64

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+System.Console.WriteLine(SeriesNumbers(1, 10));
+
+string SeriesNumbers(int start, int end)
+{
+    if(start == end)
+    {
+        return start.ToString();
+    }
+    return start + " " + (SeriesNumbers(start + 1, end));
+}
